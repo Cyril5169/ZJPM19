8a764bd1770b50fcf4a1b5b17f99f4f3d5d66aad
yanminjie-916 <55833284+yanminjie-916@users.noreply.github.com>
2020-03-20 17:06:26 +0800

    组织结构—导入模板

--- src/components/schedule/project_group.vue
+++ src/components/schedule/project_group.vue
@@ -2,11 +2,10 @@
   <div class="project_group">
     <div style="margin-bottom:10px;">
       <span>选择项目：</span>
-
       <el-select size="small" v-model="selectProjectId" placeholder="请选择项目" @change="selProject">
-        <el-option v-for="item in projectData" :key="item.p_no"
-          :label="renderFilter(item.p_no,classFilter)" :value="item.p_no"></el-option>
-            <!-- <el-option v-for="item in classFilter" :key="item.value" :label="item.display" :value="item.value">
+        <el-option v-for="item in projectData" :key="item.p_no" :label="renderFilter(item.p_no,classFilter)"
+          :value="item.p_no"></el-option>
+        <!-- <el-option v-for="item in classFilter" :key="item.value" :label="item.display" :value="item.value">
             </el-option> -->
       </el-select>
       <!-- <el-button icon="el-icon-arrow-left" size="small" style="float:right;" v-if="btnShow" @click="toProject">返回项目模板
@@ -119,11 +118,38 @@
         </el-form-item>
       </zj-form>
     </el-dialog>
+    <!-- 模板选择组织结构 -->
+    <el-dialog v-if="addFromTempGroupVisiable" width="1000px" title="选择组织模板" :close-on-click-modal="false"
+      :visible.sync="addFromTempGroupVisiable">
+      <div class="tbar">
+        <el-button icon="el-icon-refresh" title="刷新" size="mini" circle @click="searchTempGroup"></el-button>
+        <el-input size="small" @keyup.enter.native="refreshTempGroupData" placeholder="请输入模板名称"
+          v-model="TempGroupCondition" clearable style="width:250px;">
+          <el-button size="small" @click="refreshTempGroupData" slot="append" icon="el-icon-search">搜索</el-button>
+        </el-input>
+        <el-button type="primary" size="small" style="margin-left:10px;" :disabled="TempGroupSelection.length==0"
+          @click="onSelectTempGroup">
+          确认选择({{TempGroupSelection.length}})
+        </el-button>
+      </div>
+      <div class="gridTable">
+        <zj-table ref="TempGroupTable" style="width: 100%;" height="380px" :data="TempGroupData" tooltip-effect="dark"
+          highlight-current-row row-key="tg_id" default-expand-all @select="handleSelectChildren"
+          @selection-change="handleSelectionChange2" @select-all="handleSelectAll2">
+          <el-table-column type="selection" width="55" align="center"></el-table-column>
+          <el-table-column prop="wp_id" label="岗位" align="center" width="180">
+            <template slot-scope="scope">{{scope.row.wp_id | renderFilter(postDataFilter2)}}</template>
+          </el-table-column>
+          <el-table-column prop="tg_note" label="说明" align="center" show-overflow-tooltip></el-table-column>
+        </zj-table>
+      </div>
+    </el-dialog>
+
   </div>
 </template>
 
 <script>
-import member from "./project_group_member";//  ./表示当前目录下
+import member from "./project_group_member"; //  ./表示当前目录下
 // import member from "@/components/schedule/project_group_member";
 import { mapMutations } from "vuex";
 export default {
@@ -135,15 +161,21 @@ export default {
       condition: "",
       tableData: [], //表格数据
       postDataFilter: [],
+      postDataFilter2: [],
       tempGroupData: [],
       currentRow: {},
       selection: [],
       addproject_groupVisiable: false,
+      addFromTempGroupVisiable: false,
       project_groupModel: {},
       addOrNot: true, //是否新增
       addproject_groupText: "",
+      projectData: "",
       bottomDivShow: false,
       activeName: "first",
+      TempGroupSelection: "",
+      TempGroupModelList: [],
+      TempGroupCondition: "",
       add_rules: {
         wp_id: [
           { required: true, message: "请选择组织名称", trigger: "change" }
@@ -161,6 +193,7 @@ export default {
         .then(res => {
           this.classFilter = res.dict.p_no;
           this.projectData = res.data;
+          //this.console.log(projectData);
         })
         .catch(res => {});
     },
@@ -174,7 +207,7 @@ export default {
         p_no: this.selectProjectId,
         condition: this.condition
       })
-      // this.z_get("api/project_group/treeList")
+        // this.z_get("api/project_group/treeList")
         .then(res => {
           this.postDataFilter = res.dict.wp_id;
           // this.classFilter = res.dict.p_no;
@@ -183,9 +216,31 @@ export default {
         .catch(res => {});
       // }
     },
+    refreshTempGroupData() {
+      this.loading = true;
+      this.TempGroupData = [];
+      this.z_get(
+        "api/template_group/treeList",
+        {
+          condition: this.TempGroupCondition
+        },
+        { loading: false }
+      )
+        .then(res => {
+          this.loading = false;
+          this.postDataFilter2 = res.dict.wp_id;
+          this.TempGroupData = res.data;
+          this.console.log(TempGroupData);
+        })
+        .catch(res => {});
+    },
     search() {
       this.condition = "";
       this.refreshData();
+    },
+    searchTempGroup() {
+      this.TempGroupCondition = "";
+      this.refreshTempGroupData();
     },
     selProject(val) {
       this.selectProjectId = val;
@@ -195,10 +250,11 @@ export default {
     handleSelectionChange(val) {
       this.selection = val;
     },
+
     //全选选中子节点
     handleSelectAll(selection) {
       var val = this.tableData;
-      var select = false;
+      var select = false; //全选还是反选
       for (var i = 0; i < selection.length; i++) {
         if (selection[i].group_id == val[0].group_id) {
           select = true;
@@ -211,6 +267,22 @@ export default {
         }
       }
     },
+    handleSelectAll2(selection) {
+      var val = this.TempGroupData;
+      var select = false; //全选还是反选
+      for (var i = 0; i < selection.length; i++) {
+        if (selection[i].tg_id == val[0].tg_id) {
+          select = true;
+          break;
+        }
+      }
+      for (var i = 0; i < val.length; i++) {
+        if (val[i].children && val[i].children.length) {
+          this.selectChildren2(val[i].children, select);
+        }
+      }
+    },
+
     //选中子节点
     selectChildren(val, select) {
       for (var i = 0; i < val.length; i++) {
@@ -218,6 +290,19 @@ export default {
           this.$refs.project_groupTable.toggleRowSelection(val[i]);
         } else if (!select && this.selection.indexOf(val[i] > -1)) {
           this.$refs.project_groupTable.toggleRowSelection(val[i]);
+        }
+        if (val[i].children && val[i].children.length) {
+          this.selectChildren(val[i].children, select);
+        }
+      }
+    },
+
+    selectChildren2(val, select) {
+      for (var i = 0; i < val.length; i++) {
+        if (select && this.TempGroupSelection.indexOf(val[i]) == -1) {
+          this.$refs.TempGroupTable.toggleRowSelection(val[i]);
+        } else if (!select && this.TempGroupSelection.indexOf(val[i] > -1)) {
+          this.$refs.TempGroupTable.toggleRowSelection(val[i]);
         }
         if (val[i].children && val[i].children.length) {
           this.selectChildren(val[i].children, select);
@@ -370,11 +455,101 @@ export default {
         })
         .catch(res => {});
     },
+
+    //从组织模板添加
+    addFromTempGroupShow(type) {
+      var titleName = "";
+      var group_pid = null;
+      if (type == "root") {
+        titleName = "";
+        this.addproject_groupText = "新增根节点";
+      } else if (type == "children") {
+        group_pid = this.currentRow.group_id;
+        titleName = this.renderFilter(
+          this.currentRow.wp_id,
+          this.postDataFilter
+        );
+        this.addproject_groupText = "新增[" + titleName + "]的子节点";
+      }
+      this.project_groupModel = {
+        group_id: 0,
+        group_pid: group_pid,
+        p_no: this.selectTemplateId,
+        group_name: "",
+        group_note: ""
+      };
+      this.addOrNot = true;
+      this.refreshTempGroupData();
+      this.addFromTempGroupVisiable = true;
+    },
+    //确认添加任务
+    onSelectTempGroup() {
+      this.TempGroupModelList = [];
+      for (var i = 0; i < this.TempGroupSelection.length; i++) {
+        var select = this.TempGroupSelection[i];
+
+        var level = "";
+        var pid = this.project_groupModel.group_pid; //所有最顶层的节点的父节点
+        if (!select.tg_pid) {
+          //选中最顶层
+          level = "base";
+        } else {
+          level = "detail";
+          var fartherNode = this.TempGroupSelection.filter(
+            item => item.tg_id == select.tg_pid
+          ); //看是否有父节点选中，没有这个就是最顶层
+          if (fartherNode.length) {
+            pid = select.tg_pid;
+          }
+        }
+        var oneTemp = {
+          group_id: select.tg_id, //后台重新赋值
+          group_pid: pid, //后台重新赋值
+          p_no: this.selectTemplateId,
+          //tg_id: select.tg_id,
+          //dept_id: "",
+          //dept_name: "",
+          // group_name: select.tg_name,
+          group_note: select.tg_note
+        };
+        this.TempGroupModelList.push(oneTemp);
+      }
+      this.z_post("api/project_group/list", this.TempGroupModelList, {
+        params: { pid: this.project_groupModel.group_pid }
+      })
+        .then(res => {
+          this.$message({
+            message: "新增成功!",
+            type: "success",
+            duration: 1000
+          });
+          this.refreshData();
+          this.addFromTempGroupVisiable = false;
+        })
+        .catch(res => {
+          this.$alert("新增失败!", "提示", {
+            confirmButtonText: "确定",
+            type: "error"
+          });
+        });
+    },
+
     //双击选择组织
     handleSelectTreeDblClick(data) {
       this.project_groupModel.group_id = data.group_id;
       this.project_groupModel.group_name = data.group_name;
       this.$refs.select_project_group.blur();
+    },
+    //当前选中的节点
+    handleSelectionChange2(val) {
+      this.TempGroupSelection = val;
+    },
+    handleSelectChildren(selection, row) {
+      if (selection) {
+        //勾选选中子节点
+      } else {
+        //取消选中父节点
+      }
     },
     //点击组织行显示下面
     handleRowClick(row, column) {
@@ -435,6 +610,8 @@ export default {
   mounted() {
     this.refreshProjectData();
     this.refreshData();
+
+    // this.search();
   }
 };
 </script>
